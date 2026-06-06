c0828c73e5b8cdb57c9f3125ae2f0b755f54beb3
simón-2112 <simonisaza2112@gmail.com>
2026-06-06 01:04:40 -0500

    Implement home page

---
+++ frontend/js/app.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-enter").addEventListener("click", () => {
+  window.location.href = "app.html";
+});
+
+document.getElementById("btn-about").addEventListener("click", () => {
+  window.location.href = "about.html";
+});
+
+document.getElementById("btn-documentation").addEventListener("click", () => {
+  window.location.href = "documentation.html";
+});
